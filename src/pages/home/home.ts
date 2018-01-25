10a73a33d58e5c0db4de217b45946aeb8aaae719
s4dexx <nils.juenemann@outlook.com>
2018-01-25 09:45:19 +0100

    Added UI-Elements and Local Storage functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  storage: any;
+  usernameOutput: any;
+  passwordOutput: any;
+  usernameInput: any;
+  passwordInput: any;
 
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+    this.storage = window.localStorage;
   }
 
+  storeMyData() {
+    this.storage.setItem("username", this.usernameInput);
+    this.storage.setItem("password", this.passwordInput);
+  }
+
+  getMyData() {
+    this.usernameOutput = this.storage.getItem("username");
+    this.passwordOutput = this.storage.getItem("password");
+
+    let alert = this.alertCtrl.create({
+      title: 'Your saved data',
+      subTitle: 'Username: ' + this.usernameOutput + '<br>' + 'Password: ' + this.passwordOutput,
+      buttons: ['Okay']
+    });
+    alert.present();
+  }
 }
+
